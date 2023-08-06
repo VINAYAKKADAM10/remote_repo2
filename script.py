268cc72b0933a7b39ce1412c0ef4b06b3659f6b8
VINAYAKKADAM10 <125482054+VINAYAKKADAM10@users.noreply.github.com>
2023-08-06 14:49:44 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    print('10')
+
+import random
+if __name__ == "__main__":
+    print(random.randint(0,10))
+
+# this is file in remote repo
+
+# following changes were made in local comp.
+import numpy as np
+if __name__ == "__main__":
+    arr = np.array([11,21,31])
+    print(arr*11)
