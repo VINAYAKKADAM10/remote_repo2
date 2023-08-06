ac28ee3fb4a13bdfcec78c4c1db7770d41abbf12
VINAYAKKADAM10 <125482054+VINAYAKKADAM10@users.noreply.github.com>
2023-08-06 14:50:19 +0530

    Update script.py

--- script.py
+++ script.py
@@ -5,10 +5,6 @@ import random
 if __name__ == "__main__":
     print(random.randint(0,10))
 
-# this is file in remote repo
+# this is file in remote_repo2
 
-# following changes were made in local comp.
-import numpy as np
-if __name__ == "__main__":
-    arr = np.array([11,21,31])
-    print(arr*11)
+
